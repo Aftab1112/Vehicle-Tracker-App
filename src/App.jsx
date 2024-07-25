64e0e614dc33f1b35f4c4bc8d7a0faf9c6a9c5a0
Aftab1112 <you@example.com>
2024-07-26 01:30:00 +0530

    initialized empty project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-full h-full">
+      <h1 className="text-center">App</h1>
+    </div>
+  );
+};
+
+export default App;
